6567d8421e98a3f5e282c6be821808e4fd529ac0
Tobias Herber <22559657+herber@users.noreply.github.com>
2025-12-25 16:04:28 +0100

    Add ci build

--- service/src/controllers/index.ts
+++ service/src/controllers/index.ts
@@ -1,3 +1,4 @@
+import { apiMux } from '@lowerdeck/api-mux';
 import { createServer, rpcMux, type InferClient } from '@lowerdeck/rpc-server';
 import { app } from './_app';
 import { instanceController } from './instance';
@@ -17,6 +18,6 @@ export let rootController = app.controller({
 });
 
 export let forgeRPC = createServer({})(rootController);
-export let forgeApi = rpcMux({ path: '/metorial-forge' }, [forgeRPC]);
+export let forgeApi = apiMux([{ endpoint: rpcMux({ path: '/metorial-forge' }, [forgeRPC]) }]);
 
 export type ForgeClient = InferClient<typeof rootController>;
