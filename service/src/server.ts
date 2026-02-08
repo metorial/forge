4c840f0927b3758ede6fa1e0df9477b43b7e3bf0
Tobias Herber <22559657+herber@users.noreply.github.com>
2026-02-08 20:55:29 +0100

    Update service health checks and migrations

--- service/src/server.ts
+++ service/src/server.ts
@@ -1,10 +1,4 @@
-import { forgeApi } from './controllers';
-
-console.log('Server is running');
-
-Bun.serve({
-  fetch: forgeApi,
-  port: 52020
-});
-
+await import('./init');
+await import('./instrument');
+await import('./endpoints');
 await import('./worker');
